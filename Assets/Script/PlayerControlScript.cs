929e415a933db72365955bda5b9c6bd6113a3fd0
Charith Eranga <91379491+Eranga-DC@users.noreply.github.com>
2023-05-22 20:25:34 +0530

    Player Control Script
    Adding Player movement, flip direction, animation, jump function

---
+++ Assets/Script/PlayerControlScript.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControlScript : MonoBehaviour
+{
+    [SerializeField] private float _moveSpeed, _jumpForce;
+    
+    private float _horizontalInput;
+
+    private bool _isGrounded;
+    private bool _isJump;
+
+    private Rigidbody2D _playerRB;
+    private Animator _playerAnime;
+    private SpriteRenderer _playerSR;
+
+    void Start()
+    {
+        _playerRB = GetComponent<Rigidbody2D>();
+        _playerAnime = GetComponent<Animator>();
+        _playerSR = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        GetUserInput();
+        PlayerAnimation();
+    }
+
+    private void GetUserInput()
+    {
+        _horizontalInput = Input.GetAxis("Horizontal");
+        transform.position += new Vector3(_horizontalInput, 0f, 0f) * _moveSpeed * Time.deltaTime;
+
+        if (Input.GetKeyDown(KeyCode.Space) && _isGrounded)
+        {
+            _playerRB.AddForce(Vector2.up * _jumpForce, ForceMode2D.Impulse);
+            _isJump = true;
+            _isGrounded = false;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground")) {
+            _isGrounded = true;
+            _isJump = false;
+        }
+    }
+
+    private void PlayerAnimation() {
+        if (_horizontalInput > 0) {
+            _playerSR.flipX = false;
+            _playerAnime.SetBool("run",true);
+        }
+        else if (_horizontalInput < 0) {
+            _playerSR.flipX = true;
+            _playerAnime.SetBool("run", true);
+        }
+        else {
+            _playerAnime.SetBool("run", false);
+        }
+
+
+        if (_isJump)
+        {
+            _playerAnime.SetBool("jump", true);
+        }
+        else {
+            _playerAnime.SetBool("jump", false);
+        }
+
+        
+    }
+}

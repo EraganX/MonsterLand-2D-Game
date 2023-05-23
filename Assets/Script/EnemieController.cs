8561e27e84c3738242237c81f8ef790601b0f23b
Charith Eranga <91379491+Eranga-DC@users.noreply.github.com>
2023-05-23 09:43:30 +0530

    Add enemies move
    Animated enemies and add basic movement

---
+++ Assets/Script/EnemieController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemieController : MonoBehaviour
+{
+    public float EnemieMoveSpeed;
+
+    private Rigidbody2D rigid;
+
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        rigid.velocity = new Vector2(EnemieMoveSpeed,rigid.velocity.y);
+    }
+}
